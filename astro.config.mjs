1f244da3ed588d9dee71ec28bbb340bf8e092bd2
kigawa <contact@kigawa.net>
2024-05-02 19:23:21 +0000

    Updated astro configuration, added SkillCard component, and refactored styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@ import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: "@import '/src/func.scss';"
+        }
+      }
+    }
+  }
 });
